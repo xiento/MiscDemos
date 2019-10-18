8d7325319ebc716f63bafd6a57d42e76cee75e87
Patrik <patrik.bjorklund@husqvarnagroup.com>
2019-10-18 09:36:53 +0200

    toggle class on loadmore and scroll to bottom

---
+++ RoutingDemo/wwwroot/js/vehiclelist.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    var haveLoadedMore = $('body').data('have-loaded-more') === 'True';
+
+    if (haveLoadedMore) {
+        //scroll to bottom of page
+        $("html").animate({
+            scrollTop: $(document).height()
+        }, "slow", function () {
+                //animate the last 10 rows of table to show the newly added rows
+                $('table > tbody > tr').slice(-10).toggleClass('table-success', 1000);
+        });
+    }
+
+});
